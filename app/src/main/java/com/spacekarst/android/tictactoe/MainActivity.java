529468e6ee02203e1675b57c072cc15152accbe8
jskotz <jskotz@gmail.com>
2017-10-17 10:07:58 -0400

    Initial screen view using ConstraintLayout to better adjust for screen sizes. TODO: consolidate to one border.xml file

--- app/src/main/java/com/spacekarst/android/tictactoe/MainActivity.java
+++ app/src/main/java/com/spacekarst/android/tictactoe/MainActivity.java
@@ -1,13 +1,32 @@
 package com.spacekarst.android.tictactoe;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.ViewSwitcher;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        imageButton = (ImageView)findViewById(R.id.imageButton1);
+       // imageSwitcher.setImageResource(R.drawable.circle);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageButton.setImageResource(R.drawable.x);
+            }
+        });
     }
 }
